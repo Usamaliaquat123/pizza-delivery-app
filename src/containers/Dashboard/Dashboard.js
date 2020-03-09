912f7e12b161b9c60ddd830f63add05e0f78cf1c
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 13:09:10 -0700

    dashboard

---
+++ src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Dashboard </Text>
+      </View>
+    );
+  }
+}
+
+export default Dashboard;

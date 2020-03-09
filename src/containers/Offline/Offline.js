e02214ac95f293202ec2dc0dcb0f71e0eeab1055
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 13:31:28 -0700

    sty

--- src/containers/Offline/Offline.js
+++ src/containers/Offline/Offline.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import { styles } from "./Offline.styles";
 class Offline extends Component {
   constructor(props) {
     super(props);

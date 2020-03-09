afbba0076fc17be1726b02d319b8b43611d2e414
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 15:14:46 -0700

    host     : 'localhost',

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Auths from './src/navigation/AppNavigation';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Auths);
--- src/navigation/HomeNav.js
+++ src/navigation/HomeNav.js
@@ -2,12 +2,13 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import Dashboard from './../containers/Dashboard/Dashboard';
 import ProductDetail from './../containers/Dashboard/ProductDetail/ProductDetail';
+import Cart from './../containers/Dashboard/Cart/Cart';
 
  const HomeNav = createStackNavigator(
     {
         Dashboard: {screen : Dashboard},
         ProductDetail: { screen : ProductDetail },
-
+        Cart: {screen : Cart}
     }
 )
 

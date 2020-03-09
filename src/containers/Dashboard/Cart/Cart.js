88720eb08ae61685df2a8694b4cf4f32d11851df
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 13:13:30 -0700

    styleshheet

---
+++ src/containers/Dashboard/Cart/Cart.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Cart </Text>
+      </View>
+    );
+  }
+}
+
+export default Cart;

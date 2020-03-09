e9d2874892f4e497fd0aa4d8719ea7b58fdecc6f
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 13:11:39 -0700

    product detail

---
+++ src/containers/Dashboard/ProductDetail/ProductDetail.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ProductDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> ProductDetail </Text>
+      </View>
+    );
+  }
+}
+
+export default ProductDetail;
---
+++ src/containers/Dashboard/ProductDetail/ProductDetail.styles.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-natiev";
+
+export styles = StyleSheet.create({
+    
+})

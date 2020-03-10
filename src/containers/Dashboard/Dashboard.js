5e524d6ca8cb5e0cd8beb830f56d00de775f5222
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 18:59:58 -0700

    card

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Header} from 'react-native-elements';
+import {Header,Card,Button,Icon} from 'react-native-elements';
 import {View, Text, ScrollView} from 'react-native';
 import {styles} from './Dashboard.styles';
 import Colors from './../../theme/Colors';
@@ -20,23 +20,78 @@ class Dashboard extends Component {
           rightComponent={{icon: 'home', color: '#fff'}}
         />
         <ScrollView>
-          <Text
-            style={{
-              textAlign: 'center',
-              color: Colors.theme_color.orange,
-              fontWeight: 'bold',
-              fontSize: 40,
-            }}>
-            Food Menu
-          </Text>
-          <View
-            style={{
-              borderBottomColor: Colors.theme_color.orange,
-              borderBottomWidth: 1,
-              width: 200,
-              alignSelf: 'center',
-            }}
-          />
+          <View>
+            <Text
+              style={{
+                textAlign: 'center',
+                color: Colors.theme_color.orange,
+                fontWeight: 'bold',
+                fontSize: 40,
+              }}>
+              Food Menu
+            </Text>
+            <View
+              style={{
+                borderBottomColor: Colors.theme_color.orange,
+                borderBottomWidth: 1,
+                width: 200,
+                alignSelf: 'center',
+              }}
+            />
+
+            <Text
+              style={{
+                textAlign: 'center',
+                fontSize: 17,
+                color: '#322514',
+                margin: 10,
+                opacity: 0.6,
+              }}>
+              Choose your best pizza have a great day !
+            </Text>
+          </View>
+          {/* Featured Pizza's */}
+
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}>
+            <Card
+  title='HELLO WORLD'
+  // image={require('../images/pic2.jpg')}
+  >
+  <Text style={{marginBottom: 10}}>
+    The idea with React Native Elements is more about component structure than actual design.
+  </Text>
+  <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+</Card>
+               <Card
+  title='HELLO WORLD'
+  // image={require('../images/pic2.jpg')}
+  >
+  <Text style={{marginBottom: 10}}>
+    The idea with React Native Elements is more about component structure than actual design.
+  </Text>
+  <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+</Card>
+   <Card
+  title='HELLO WORLD'
+  // image={require('../images/pic2.jpg')}
+  >
+  <Text style={{marginBottom: 10}}>
+    The idea with React Native Elements is more about component structure than actual design.
+  </Text>
+  <Button
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title='VIEW NOW' />
+</Card>
+            </ScrollView>
         </ScrollView>
       </View>
     );

1906f44bf53be9ce888c7f6043c19ff110417f5d
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-09 18:50:30 -0700

    align

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react';
-import { Header } from "react-native-elements";
-import { View, Text,ScrollView } from 'react-native';
-import {styles} from './Dashboard.styles'
+import React, {Component} from 'react';
+import {Header} from 'react-native-elements';
+import {View, Text, ScrollView} from 'react-native';
+import {styles} from './Dashboard.styles';
 import Colors from './../../theme/Colors';
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <View>
-      <Header
-  backgroundColor="#fff"
-  placement="center"
-  leftComponent={{ icon: 'menu', color: Colors.theme_color.orange }}
-  // centerComponent={{ text: 'MY TITLE', style: { color: Colors.theme_color.orange, fontWeight: "bold" } }}
-  rightComponent={{ icon: 'home', color: '#fff' }}
-/>
-      <ScrollView>
-      
-        <Text>asda</Text>
-      </ScrollView>
+        <Header
+          backgroundColor="#fff"
+          placement="center"
+          leftComponent={{icon: 'menu', color: Colors.theme_color.orange}}
+          // centerComponent={{ text: 'MY TITLE', style: { color: Colors.theme_color.orange, fontWeight: "bold" } }}
+          rightComponent={{icon: 'home', color: '#fff'}}
+        />
+        <ScrollView>
+          <Text
+            style={{
+              textAlign: 'center',
+              color: Colors.theme_color.orange,
+              fontWeight: 'bold',
+              fontSize: 40,
+            }}>
+            Food Menu
+          </Text>
+          <View
+            style={{
+              borderBottomColor: Colors.theme_color.orange,
+              borderBottomWidth: 1,
+              width: 200,
+              alignSelf: 'center',
+            }}
+          />
+        </ScrollView>
       </View>
     );
   }

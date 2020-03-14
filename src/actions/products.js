e1f1bee1bd7d6deed0c1bb87bc2b0f213e90af78
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-13 17:00:50 -0700

    act

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,8 +1,28 @@
 import Api from './../Services/Api';
-import { ADD_FEATURED_PRODUCTS,ADD_NORMAL_PRODUCTS } from './types';
+import { ADD_FEATURED_PRODUCTS, ADD_NORMAL_PRODUCTS, SET_ERROR } from './types';
 
 
 
 export function setAllFeaturedProducts() {
-    
+    return dispatch => {
+        Api.products().then(products => dispatch({
+            type: ADD_FEATURED_PRODUCTS,
+            payload: products.data.filter(data => data.featured == 1)
+        })).catch(err => dispatch({
+            type: SET_ERROR,
+            error: err
+        }))
+    }
+}
+
+export function setAllNormalProducts(){
+    return dispatch => {
+        Api.producs().then(products => dispatch({
+            type: ADD_NORMAL_PRODUCTS,
+            payload: products.data.filter(data => data.featured == 0)
+        })).catch(err => dispatch({
+            type: SET_ERROR,
+            error: err
+        }))
+    }
 }

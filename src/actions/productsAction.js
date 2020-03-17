98a951495a4e77401daaf5c639c2ad75bdb29c61
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-17 12:31:49 -0700

    sd

--- src/actions/productsAction.js
+++ src/actions/productsAction.js
@@ -5,17 +5,11 @@ import { ADD_FEATURED_PRODUCTS, ADD_NORMAL_PRODUCTS, SET_ERROR } from './types';
 
 export function setAllFeaturedProducts() {
     return dispatch => {
-        console.log();
-        
-        Api.products().then(products => {
-            console.log(products);
-            
+        Api.products().then(products => 
             dispatch({
                 type: ADD_FEATURED_PRODUCTS,
                 payload: products.data
-            })
-            
-            })
+            }),)
         // return Api.products().then(products => {
         // products.data.filter(data => data.featured == 1)
         // for (let i = 0; i < arr.length; i++) {
@@ -29,20 +23,21 @@ export function setAllFeaturedProducts() {
         //     type: SET_ERROR,
         //     error: err
         // }))
-    }
+    };
 }
 export function setAllNormalProducts(){
     return dispatch => {
-        // Api.producs().then(products => {
-        // products.data.filter(data => data.featured == 0)
+        Api.producs().then(products => 
+                // products.data.filter(data => data.featured == 0)
         // for (let i = 0; i < arrs.length; i++) {
         //   arrs[i]['cart'] = false
         // }
-        //     dispatch({
-        //     type: ADD_NORMAL_PRODUCTS,
-        //     payload: products.data.filter(data => data.featured == 0)
-        // })
-        // }).catch(err => dispatch({
+            dispatch({
+            type: ADD_NORMAL_PRODUCTS,
+            payload: products.data.filter(data => data.featured == 0)
+        })
+        )
+        // .catch(err => dispatch({
         //     type: SET_ERROR,
         //     error: err
         // }))

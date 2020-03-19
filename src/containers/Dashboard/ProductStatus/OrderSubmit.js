9cbfe6505467256df30ab898fcb9cc10c5c94b23
Usamaliaquat123 <inconnent4@gmail.com>
2020-03-19 12:07:48 -0700

    sumit

--- src/containers/Dashboard/ProductStatus/OrderSubmit.js
+++ src/containers/Dashboard/ProductStatus/OrderSubmit.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export class OrderPlaced extends Component {
+export class OrderSubmit extends Component {
     render() {
         return (
             <View>
@@ -11,4 +11,4 @@ export class OrderPlaced extends Component {
     }
 }
 
-export default OrderPlaced
+export default OrderSubmit
--- src/navigation/HomeNav.js
+++ src/navigation/HomeNav.js
@@ -3,12 +3,21 @@ import {createAppContainer} from 'react-navigation';
 import Dashboard from './../containers/Dashboard/Dashboard';
 import ProductDetail from './../containers/Dashboard/ProductDetail/ProductDetail';
 import Cart from './../containers/Dashboard/Cart/Cart';
+import { OrderCompleted } from './../containers/Dashboard/ProductStatus/OrderCompleted';
+import { OrderPlaced } from './../containers/Dashboard/ProductStatus/OrderPlaced';
+import { OrderSubmit } from './../containers/Dashboard/ProductStatus/OrderSubmit';
+import { OrderProcessing } from './../containers/Dashboard/ProductStatus/OrderProcessing';
+import { OrderRejected } from './../containers/Dashboard/ProductStatus/OrderRejected';
 
 const HomeNav = createStackNavigator(
   {
     Dashboard: {screen: Dashboard},
     ProductDetail: {screen: ProductDetail},
     Cart: {screen: Cart},
+    OrderCompleted: { screen: OrderCompleted },
+    OrderSubmit:  { screen : OrderSubmit },
+    OrderProcessing: { screen : OrderProcessing },
+    OrderRejected: { screen : OrderRejected }
   },
   {
     headerMode: 'none',
